d3f3568767f8a4f0026f19a83e08986fe84982f0
Saler C. Lourenço <saler.cez@gmail.com>
2024-02-22 20:31:10 -0300

    cria classes no controller e no service

---
+++ src/main/java/app/controller/AutorController.java
@@ -0,0 +1,10 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/autor")
+public class AutorController {
+
+}
